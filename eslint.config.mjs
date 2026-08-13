95af4885b43c8b21a56fa8c3e64f4509ed8f0ba5
Your Full Name <harshal@mamotechnolabs.com>
2026-08-13 21:22:26 +0530

    added linter

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
+
+export default defineConfig([
+  {
+    ignores: ['node_modules/**', 'dist/**', 'coverage/**'],
+  },
+  {
+    files: ['**/*.{js,ts}'],
+    extends: [js.configs.recommended, tseslint.configs.recommended],
+  },
+]);
